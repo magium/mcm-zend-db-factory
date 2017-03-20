9fd3c47202bdcb86a49d5d1e17dd2ee17e5f0386
Kevin Schroeder <kschroeder@mirageworks.com>
2017-03-20 13:44:02 -0500

    Should be working now

--- test/ConfigurationTest.php
+++ test/ConfigurationTest.php
@@ -3,6 +3,7 @@
 namespace Magium\ZendDbFactory\Tests;
 
 use Magium\Configuration\Config\ConfigurationRepository;
+use Magium\ZendDbFactory\InvalidConfigurationException;
 use Magium\ZendDbFactory\ZendDbFactory;
 use PHPUnit\Framework\TestCase;
 use Zend\Db\Adapter\Adapter;
@@ -15,16 +16,32 @@ class ConfigurationTest extends TestCase
         $configuration = new ConfigurationRepository(<<<XML
 <config>
     <database>
-        <adapter>
+        <zenddb>
             <driver>pdo_sqlite</driver>
             <hostname>:memory:</hostname>
-        </adapter>
+        </zenddb>
     </database>
 </config>
 XML
         );
         $adapter = ZendDbFactory::factory($configuration);
         self::assertInstanceOf(Adapter::class, $adapter);
+    }
+
+    public function testMissingDriverThrowsException()
+    {
+        $this->expectException(InvalidConfigurationException::class);
+        $configuration = new ConfigurationRepository(<<<XML
+<config>
+    <database>
+        <zenddb>
+            <hostname>:memory:</hostname>
+        </zenddb>
+    </database>
+</config>
+XML
+        );
+        ZendDbFactory::factory($configuration);
     }
 
 }
